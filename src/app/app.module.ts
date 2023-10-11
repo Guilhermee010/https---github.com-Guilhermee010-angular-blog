7d04b0c504be3ffe62038f013589ac4023526c32
guilhermee010 <augustogarcia.ambrosio@gmail.com>
2023-10-10 23:14:37 -0300

    criando os elementos do menu-title e big-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { BigCardsComponent } from './components/big-cards/big-cards.component';
+import { SmallCardsComponent } from './components/small-cards/small-cards.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    BigCardsComponent,
+    SmallCardsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
